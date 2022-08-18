1a659174b5605d10e48af1092411d5e880e5ec15
Akiranya <porridgen@gmail.com>
2022-08-18 23:46:53 +0800

    Replace ACF with CommandAPI

---
+++ src/main/java/net/leonardo_dgs/interactivebooks/CommandIBooksNew.java
@@ -0,0 +1,122 @@
+package net.leonardo_dgs.interactivebooks;
+
+import dev.jorel.commandapi.CommandAPICommand;
+import dev.jorel.commandapi.arguments.*;
+import net.leonardo_dgs.interactivebooks.util.PAPIUtil;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.concurrent.CompletableFuture;
+
+public class CommandIBooksNew {
+
+    private static final ArgumentSuggestions IBOOKS;
+
+    static {
+        IBOOKS = ArgumentSuggestions.stringsAsync(info -> CompletableFuture.supplyAsync(() ->
+                InteractiveBooks.getBooks().keySet().stream().map(book -> "\"" + book + "\"").toArray(String[]::new))
+        );
+    }
+
+    public CommandIBooksNew() {
+        new CommandAPICommand("ibooks")
+                .withAliases("interactivebooks", "ibooks", "ib")
+                .withPermission("interactivebooks.command")
+                .withSubcommand(new CommandAPICommand("list")
+                        .withPermission("interactivebooks.command.list")
+                        .executes(((sender, args) -> {
+                            StringBuilder sb = new StringBuilder();
+                            Iterator<String> iterator = InteractiveBooks.getBooks().keySet().iterator();
+                            boolean hasNext = iterator.hasNext();
+                            while (hasNext) {
+                                String bookId = iterator.next();
+                                sb.append("§6");
+                                sb.append(bookId);
+                                hasNext = iterator.hasNext();
+                                if (hasNext)
+                                    sb.append("§7, ");
+                            }
+                            sender.sendMessage("§eBooks:\n" + sb);
+                        })))
+                .withSubcommand(new CommandAPICommand("open")
+                        .withPermission("interactivebooks.command.open")
+                        .withArguments(new TextArgument("book-id").replaceSuggestions(IBOOKS))
+                        .withArguments(new EntitySelectorArgument<Player>("player", EntitySelector.ONE_PLAYER))
+                        .executes((sender, args) -> {
+                            Player playerToOpen = (Player) args[1];
+                            String bookIdToOpen = PAPIUtil.setPlaceholders(playerToOpen, (String) args[0]);
+                            if (InteractiveBooks.getBook(bookIdToOpen) == null) {
+                                sender.sendMessage("§cThat book doesn't exists.");
+                                return;
+                            }
+                            if (playerToOpen == null) {
+                                sender.sendMessage("§cThat player isn't connected.");
+                                return;
+                            }
+                            InteractiveBooks.getBook(bookIdToOpen).open(playerToOpen);
+                            if (!playerToOpen.equals(sender))
+                                sender.sendMessage("§aBook §6%book_id% §aopened to §6%player%§a.".replace("%book_id%", bookIdToOpen).replace("%player%", playerToOpen.getName()));
+                        }))
+                .withSubcommand(new CommandAPICommand("get")
+                        .withPermission("interactivebooks.command.get")
+                        .withArguments(new TextArgument("book-id").replaceSuggestions(IBOOKS))
+                        .executesPlayer((player, args) -> {
+                            String bookIdToGet = PAPIUtil.setPlaceholders(player, (String) args[0]);
+                            if (InteractiveBooks.getBook(bookIdToGet) == null) {
+                                player.sendMessage("§cThat book doesn't exists.");
+                                return;
+                            }
+                            player.getInventory().addItem(InteractiveBooks.getBook(bookIdToGet).getItem(player));
+                            player.sendMessage("§aYou have received the book §6%book_id%§a.".replace("%book_id%", bookIdToGet));
+                        }))
+                .withSubcommand(new CommandAPICommand("give")
+                        .withPermission("interactivebooks.command.give")
+                        .withArguments(new TextArgument("book-id").replaceSuggestions(IBOOKS))
+                        .withArguments(new EntitySelectorArgument<Player>("player", EntitySelector.ONE_PLAYER))
+                        .executes((sender, args) -> {
+                            Player targetPlayer = (Player) args[1];
+                            String targetBookId = PAPIUtil.setPlaceholders(targetPlayer, (String) args[0]);
+                            if (InteractiveBooks.getBook(targetBookId) == null) {
+                                sender.sendMessage("§cThat book doesn't exists.");
+                                return;
+                            }
+                            if (targetPlayer == null) {
+                                sender.sendMessage("§cThat player isn't connected.");
+                                return;
+                            }
+                            targetPlayer.getInventory().addItem(InteractiveBooks.getBook(targetBookId).getItem(targetPlayer));
+                            sender.sendMessage("§aBook §6%book_id% §agiven to §6%player%§a.".replace("%book_id%", targetBookId).replace("%player%", (String) args[1]));
+                            targetPlayer.sendMessage("§aYou have received the book §6%book_id%§a.".replace("%book_id%", targetBookId));
+                        }))
+                .withSubcommand(new CommandAPICommand("create")
+                        .withPermission("interactivebooks.command.create")
+                        .withArguments(new TextArgument("book-id"))
+                        .withArguments(new TextArgument("name"))
+                        .withArguments(new TextArgument("title"))
+                        .withArguments(new TextArgument("author"))
+                        .withArguments(new MultiLiteralArgument("ORIGINAL", "COPY_OF_ORIGINAL", "COPY_OF_COPY", "TATTERED"))
+                        .executes((sender, args) -> {
+                            if (InteractiveBooks.getBook((String) args[0]) != null) {
+                                sender.sendMessage("§cA book with that id already exists");
+                                return;
+                            }
+                            String bookId = (String) args[0];
+                            String bookName = (String) args[1];
+                            String bookTitle = (String) args[2];
+                            String bookAuthor = (String) args[3];
+
+                            IBook createdBook = new IBook(bookId, bookName, bookTitle, bookAuthor, (String) args[4], new ArrayList<>(), new ArrayList<>());
+                            createdBook.save();
+                            InteractiveBooks.registerBook(createdBook);
+                            sender.sendMessage("§aBook successfully created.");
+                        }))
+                .withSubcommand(new CommandAPICommand("reload")
+                        .withPermission("interactivebooks.command.reload")
+                        .executes((sender, args) -> {
+                            ConfigManager.loadAll();
+                            sender.sendMessage("§aConfig reloaded!");
+                        }))
+                .register();
+    }
+}
--- src/main/java/net/leonardo_dgs/interactivebooks/InteractiveBooks.java
+++ src/main/java/net/leonardo_dgs/interactivebooks/InteractiveBooks.java
@@ -1,7 +1,5 @@
 package net.leonardo_dgs.interactivebooks;
 
-import co.aikar.commands.CommandReplacements;
-import co.aikar.commands.PaperCommandManager;
 import net.kyori.adventure.platform.bukkit.BukkitAudiences;
 import net.leonardo_dgs.interactivebooks.util.MinecraftVersion;
 import org.bstats.bukkit.Metrics;
@@ -18,7 +16,58 @@ public final class InteractiveBooks extends JavaPlugin {
     private static InteractiveBooks instance;
     private static final Map<String, IBook> books = new HashMap<>();
     private BukkitAudiences adventure;
-    private static PaperCommandManager commandManager;
+
+    /**
+     * Gets the instance of this plugin.
+     *
+     * @return an instance of the plugin
+     */
+    public static InteractiveBooks getInstance() {
+        return instance;
+    }
+
+    /**
+     * Gets the registered books.
+     *
+     * @return a {@link Map} with book ids as keys and the registered books
+     * ({@link IBook}) as values
+     */
+    public static Map<String, IBook> getBooks() {
+        return Collections.unmodifiableMap(books);
+    }
+
+    /**
+     * Gets an {@link IBook} by its id.
+     *
+     * @param id the id of the book to get
+     * @return the book with the specified id if it's registered, or null if not
+     * found
+     * @see #registerBook(IBook)
+     */
+    public static IBook getBook(String id) {
+        return books.get(id);
+    }
+
+    /**
+     * Registers a book.
+     *
+     * @param book the book id to register
+     */
+    public static void registerBook(IBook book) {
+        books.put(book.getId(), book);
+    }
+
+    /**
+     * Unregisters a book by its id.
+     *
+     * @param id the book id to unregister
+     */
+    public static void unregisterBook(String id) {
+        IBook book = getBook(id);
+        if (book != null) {
+            books.remove(id);
+        }
+    }
 
     @Override
     public void onEnable() {
@@ -47,75 +96,12 @@ public final class InteractiveBooks extends JavaPlugin {
         instance = null;
     }
 
-    /**
-     * Gets the instance of this plugin.
-     *
-     * @return an instance of the plugin
-     */
-    public static InteractiveBooks getInstance() {
-        return instance;
-    }
-
-    /**
-     * Gets the registered books.
-     *
-     * @return a {@link Map} with book ids as keys and the registered books ({@link IBook}) as values
-     */
-    public static Map<String, IBook> getBooks() {
-        return Collections.unmodifiableMap(books);
-    }
-
-    /**
-     * Gets an {@link IBook} by its id.
-     *
-     * @param id the id of the book to get
-     * @return the book with the specified id if it's registered, or null if not found
-     * @see #registerBook(IBook)
-     */
-    public static IBook getBook(String id) {
-        return books.get(id);
-    }
-
-    /**
-     * Registers a book.
-     *
-     * @param book the book id to register
-     */
-    public static void registerBook(IBook book) {
-        if (!book.getOpenCommands().isEmpty()) {
-            CommandReplacements replacements = commandManager.getCommandReplacements();
-            replacements.addReplacement("openbook", String.join("|", book.getOpenCommands()));
-            replacements.addReplacement("interactivebooks.open", "interactivebooks.open." + book.getId());
-            CommandOpenBook command = new CommandOpenBook(book);
-            commandManager.registerCommand(command);
-            book.setCommandExecutor(command);
-        }
-        books.put(book.getId(), book);
-    }
-
-    /**
-     * Unregisters a book by its id.
-     *
-     * @param id the book id to unregister
-     */
-    public static void unregisterBook(String id) {
-        IBook book = getBook(id);
-        if (book != null) {
-            if (book.getCommandExecutor() != null)
-                commandManager.unregisterCommand(book.getCommandExecutor());
-            books.remove(id);
-        }
-    }
-
     public BukkitAudiences adventure() {
         return adventure;
     }
 
     private void registerCommands() {
-        commandManager = new PaperCommandManager(this);
-        commandManager.getCommandCompletions().registerCompletion("ibooks", handler -> getBooks().keySet());
-        commandManager.getCommandCompletions().registerStaticCompletion("book_generations", new String[]{"original", "copy_of_original", "copy_of_copy", "tattered"});
-        commandManager.registerCommand(new CommandIBooks());
+        new CommandIBooksNew();
     }
 
 }
