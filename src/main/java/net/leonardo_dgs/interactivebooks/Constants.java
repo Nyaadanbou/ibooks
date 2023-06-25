94f42d20bc40c5333d87113ea843dc7f212a21f4
Akiranya <porridgen@gmail.com>
2023-06-25 10:40:39 +0800

    Add TODO: migrate to PDC and remove NBT-API dependency

--- src/main/java/net/leonardo_dgs/interactivebooks/Constants.java
+++ src/main/java/net/leonardo_dgs/interactivebooks/Constants.java
@@ -1,5 +1,6 @@
 package net.leonardo_dgs.interactivebooks;
 
 public class Constants {
+    // TODO Use PDC to store book ids
     public static final String BOOK_ID_KEY = "InteractiveBooks|Book-Id";
 }
