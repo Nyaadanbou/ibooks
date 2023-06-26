668bc819150118823951493fe0c13cf8f03b31a8
Akiranya <porridgen@gmail.com>
2023-06-26 18:50:46 +0800

    chore: import project InteractiveBooks

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,128 +1,57 @@
+import net.minecrell.pluginyml.paper.PaperPluginDescription
+
 plugins {
-    `java-library`
-    `maven-publish`
-    id("com.github.johnrengelman.shadow") version "7.1.2"
-    id("net.minecrell.plugin-yml.bukkit") version "0.5.2"
-    id("net.kyori.indra") version "2.1.1"
-    id("net.kyori.indra.git") version "2.1.1"
+    id("cc.mewcraft.java-conventions")
+    id("cc.mewcraft.deploy-conventions")
+    id("cc.mewcraft.repository-conventions")
+    id("cc.mewcraft.paper-plugins")
+    alias(libs.plugins.pluginyml.paper)
 }
+
+project.ext.set("name", "InteractiveBooks")
 
 group = "net.leonardo_dgs"
 version = "1.6.5"
 description = "Create cool interactive books!"
 
-repositories {
-    mavenCentral()
-    maven("https://repo.papermc.io/repository/maven-public/") {
-        content {
-            includeGroup("io.papermc.paper")
-            includeGroup("net.md-5")
-        }
-    }
-    maven("https://repo.codemc.org/repository/maven-public/") {
-        content {
-            includeGroup("de.tr7zw")
-        }
-    }
-    maven("https://repo.extendedclip.com/content/repositories/placeholderapi") {
-        content {
-            includeGroup("me.clip")
-        }
-    }
-    maven("https://jitpack.io") {
-        content {
-            includeGroup("com.github.Simplix-Softworks")
-        }
-    }
-}
-
 dependencies {
-    // API
-    compileOnly("io.papermc.paper", "paper-api", "1.19.4-R0.1-SNAPSHOT")
-    compileOnly("org.jetbrains", "annotations", "23.0.0")
+    compileOnly(project(":mewcore"))
+
+    // server api
+    compileOnly(libs.server.paper)
+
+    // plugin libraries
+    compileOnly(libs.helper)
+    compileOnly(libs.papi)
+
+    // compile time stuff
     compileOnly("org.projectlombok", "lombok", "1.18.24")
     annotationProcessor("org.projectlombok", "lombok", "1.18.24")
-
-    // Plugin libraries
-    compileOnly("me.lucko", "helper", "5.6.10")
-    compileOnly("me.clip", "placeholderapi", "2.11.3")
-
-    // Libraries that needs to be shaded
-    implementation("de.tr7zw", "item-nbt-api", "2.11.2")
-    implementation("com.github.Simplix-Softworks", "SimplixStorage", "3.2.5")
-    implementation("org.bstats", "bstats-bukkit", "3.0.0")
-    val cloudVersion = "1.8.0"
-    implementation("cloud.commandframework", "cloud-paper", cloudVersion)
-    implementation("cloud.commandframework", "cloud-minecraft-extras", cloudVersion) {
-        exclude("net.kyori")
-    }
 }
 
-indra {
-    javaVersions().target(17)
-}
-
-bukkit {
+paper {
     main = "net.leonardo_dgs.interactivebooks.InteractiveBooks"
-    name = project.name
+    name = project.ext.get("name") as String
     version = "${project.version}"
     description = project.description
     apiVersion = "1.19"
-    depend = listOf("helper")
-    softDepend = listOf("PlaceholderAPI")
-    authors = listOf("Leonardo_DGS")
-    website = "https://www.spigotmc.org/resources/45604/"
-}
+    authors = listOf("Nailm", "Leonardo_DGS")
 
-tasks {
-    jar {
-        archiveClassifier.set("noshade")
-    }
-    build {
-        dependsOn(shadowJar)
-    }
-    shadowJar {
-        minimize()
-        archiveFileName.set("${project.name}-${project.version}.jar")
-        archiveClassifier.set("")
-        sequenceOf(
-            "de.tr7zw",
-            "org.bstats",
-            "de.leonhard.storage",
-            "cloud.commandframework",
-            "io.leangen.geantyref"
-        ).forEach {
-            relocate(it, "net.leonardo_dgs.interactivebooks.lib.$it")
+    serverDependencies {
+        register("helper") {
+            required = true
+            joinClasspath = true
+            load = PaperPluginDescription.RelativeLoadOrder.OMIT
         }
-    }
-//    processResources {
-//        val tokens = mapOf(
-//            "project.version" to project.version
-//        )
-//        inputs.properties(tokens)
-//    }
-    task("deploy") {
-        dependsOn(build)
-        doLast {
-            exec {
-                commandLine("rsync", "${shadowJar.get().archiveFile.get()}", "dev:data/dev/jar")
-            }
+        register("MewCore") {
+            required = true
+            joinClasspath = true
+            load = PaperPluginDescription.RelativeLoadOrder.OMIT
+        }
+        register("PlaceholderAPI") {
+            required = false
+            joinClasspath = true
+            load = PaperPluginDescription.RelativeLoadOrder.OMIT
         }
     }
 }
-
-java {
-    withSourcesJar()
-    withJavadocJar()
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            from(components["java"])
-        }
-    }
-}
-
-fun lastCommitHash(): String = indraGit.commit()?.name?.substring(0, 7) ?: error("Could not determine commit hash")
-fun String.decorateVersion(): String = if (endsWith("-SNAPSHOT")) "$this+${lastCommitHash()}" else this
